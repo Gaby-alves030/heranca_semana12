853460a62a722cbd12a280b84e6d602f2c85e7c7
Gaby <gaabyrocha574@gmail.com>
2021-10-28 15:31:45 +0000

    atividade 12

---
+++ Administrador.java
@@ -0,0 +1,21 @@
+public class Administrador extends Empregado {
+    
+    private double ajudaDeCustos;
+
+    public void setAjudaDeCustos(double ajudaDeCustos){
+        this.ajudaDeCustos = ajudaDeCustos;
+    }
+
+
+    public double getAjudaDeCustos(){
+        return this.ajudaDeCustos;
+    }
+
+    public double calcularSalario(){
+        return this.salarioBase + this.imposto + this.ajudaDeCustos; 
+    }
+
+
+}
+    
+
--- Pessoa.java
+++ Pessoa.java
@@ -1,51 +1,45 @@
-//nome da classe
-public class Pessoa{
+public class Pessoa {
 
-    //atributos
-private String nome;
-private String endereço;
-private String telefone;
+    private String nome;
+    private String endereco;
+    private String telefone;
 
-//método de alteração
-public void setNome(String nome){
-    this.nome=nome;
-}
-//método de consulta
-public String getNome(){
-    return this.nome;
-     
-}
-//método de alteração
-public void setENdereco(String endereço){
-    this.endereço=endereço;
-}
-//método de consulta
-public String getEndereço(){
-    return this.endereço;
-}
-    //método de alteração
-    public void setTelefone(String telefone){
-        this.telefone=telefone;
-
-        //método de consulta
-        String getTelefone(){
-            return this.telefone;
-        }
-
-        
+    public void setNome(String nome){
+        this.nome = nome;
     }
-//construtor padrão
-public Pessoa(){
 
-}
-//construtor com um parametro
-public Pessoa(String nome){
-    this.nome=nome;
-
-}
-//construtor com dois parametros
-    public Pessoa(String nome,String endereço){
-        this.nome=nome;
-        this.endereço=endereço;
+    public String getNome(){
+     return this.nome;
     }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getEndereco(){
+        return this.endereco;
+    }
+
+    public void setTelefone(){
+        this.telefone = telefone;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
+    }
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    public Pessoa(String telefone, String endereco){
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+
 }
